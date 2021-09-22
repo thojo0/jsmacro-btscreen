0e5fe1a913a1ce267837acc0a6d1cdba2692b745
Daniel Mayovskiy <comepot.4anell@gmail.com>
2021-09-22 11:28:59 -0700

    more organizing

--- index.js
+++
@@ -1,7 +0,0 @@
-// I'll just write notes here from now on
-
-// commands that I don't have permissions for:
-// //move
-// any selection command at all, like //sel type, //deselect, //hpos1, //hpos2, //farwand
-// //cut
-// which is very weird
